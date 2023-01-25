f25d84551e0d93be3aa235c274c9b2dd092f9787
Rajesh Shitap <rajms1977@gmail.com>
2023-01-25 20:19:01 +0530

    modified:   src/DataStreamer.ts 	modified:   src/Graph.tsx

--- src/DataStreamer.ts
+++ src/DataStreamer.ts
@@ -3,29 +3,32 @@ export interface Order {
   size: number,
 }
 export interface ServerRespond {
-  stock: string,
-  top_bid: Order,
-  top_ask: Order,
+  price_abc: number,
+  price_def: number,
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number,
 }
 
-class DataStreamer {
-  static API_URL: string = 'http://localhost:8080/query?id=1';
+export class DataManipulator {
+  static generateRow(ServerRespond: ServerRespond[]): Row {
+    const priceABC = (ServerRespond[0].top_ask.price + ServerRespond[0].top_bid.price) / 2;
+    const priceDEF = (ServerRespond[1].top_ask.price + ServerRespond[1].top_bid.price) / 2;
+    const ratio = priceABC / priceDEF;
+    const upperBound = 1 + 0.05;
+    const lowerBound = 1 - 0.05;
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      timestamp: ServerRespond[0].timestamp > ServerRespond[1].timestamp ?
+        ServerRespond[0].timestamp : ServerRespond[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined,
 
-  static getData(callback: (data: ServerRespond[]) => void): void {
-    const request = new XMLHttpRequest();
-    request.open('GET', DataStreamer.API_URL, false);
-
-    request.onload = () => {
-      if (request.status === 200) {
-        callback(JSON.parse(request.responseText));
-      } else {
-        alert ('Request failed');
-      }
-    }
-
-    request.send();
+    };
   }
 }
-
-export default DataStreamer;
